fb162ed79e8bc1531c9332b3a30c7b272c7bd16f
Scott Greenup <scott.j.greenup@gmail.com>
2015-02-13 02:18:09 +1100

    Flask app with bootstrap template, no content.

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+    Fast Card
+
+    A wikipedia like system where information is kept in a card format. The main
+    audience for this project are individuals who repeatably refer to references
+    online. Those references can be pulled in and the data stored as a card, and
+    can then be easily searched.
+
+    It is akin to a mind palace, but on the internet.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
